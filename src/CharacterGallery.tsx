113f71cfd88a651027602a66a330d84d986800f9
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-09-28 14:25:32 +0200

    Add input for search

--- src/CharacterGallery.tsx
+++ src/CharacterGallery.tsx
@@ -1,5 +1,6 @@
 import {Character} from "./characters.ts";
 import CharacterCard from "./CharacterCard.tsx";
+import {ChangeEvent, useState} from "react";
 
 type CharacterGalleryProps = {
     characters: Character[],
@@ -8,12 +9,21 @@ type CharacterGalleryProps = {
 
 export default function CharacterGallery(props: CharacterGalleryProps) {
 
+    const [searchText, setSearchText] = useState<string>("")
+
+    const filteredCharacters = props.characters.filter((character) => character.name.toLowerCase().includes(searchText.toLowerCase()))
+
+    function onSearchTextChange(event: ChangeEvent<HTMLInputElement>) {
+        setSearchText(event.target.value)
+    }
+
     return (
         <div>
             <p>
                 Gallery
             </p>
-            {props.characters.map(character => <CharacterCard key={character.id} character={character}/>)}
+            <input onChange={onSearchTextChange} value={searchText}/>
+            {filteredCharacters.map(character => <CharacterCard key={character.id} character={character}/>)}
         </div>
     )
 }
