aa69832538ee7d167be262701e04f12638f50df2
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-10-02 10:39:02 +0200

    Add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import TopNavigation from "./TopNavigation.tsx";
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./HomePage.tsx";
 import CharacterGallery from "./CharacterGallery.tsx";
-import {Character, charactersResponse} from "./characters.ts";
+import {Character} from "./characters.ts";
 import CharacterDetails from "./CharacterDetails.tsx";
 
 import AddCharacter from "./AddCharacter.tsx";
+import axios from "axios";
 
 export default function App() {
-    const [characters, setCharacters] = useState<Character[]>(charactersResponse.results);
+    const [characters, setCharacters] = useState<Character[]>([]);
 
+    useEffect(() => {
+        console.log("useEffect")
+        loadCharacters()
+    }, [])
+
+    function loadCharacters() {
+        axios.get("https://rickandmortyapi.com/api/character")
+            .then((response) => {
+                console.log("then 1")
+
+                return response.data.results
+            })
+            .then((results) => {
+                console.log("then 2")
+                setCharacters(results)
+            })
+            .catch((reason) => {
+                console.error("fehler")
+            })
+            .finally(() => {
+                console.log("fertig")
+            })
+    }
 
     function saveCharacter(characterToSave: Character) {
         setCharacters([...characters, characterToSave])
