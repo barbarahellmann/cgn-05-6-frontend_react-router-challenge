3a100cbe8434465c1a551714f0e4e9f2df210e8d
barbarahellmann <babshellmann@web.de>
2024-04-18 11:59:12 +0200

    Integrated first pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import './App.css'
-import {useState} from "react";
+import React, {useState} from "react";
 import {Character, charactersResponse} from "./characters.ts";
 import CharacterGallery from "./CharacterGallery.tsx";
+import Navigation from "./components/Navigation.tsx";
+import {Route, Routes} from "react-router-dom";
+import Homepage from "./pages/homepage.tsx";
+import Characters from "./pages/characters.tsx";
 
 function App() {
 
     const [characters, setCharacters] = useState<Character[]>(charactersResponse.results)
-
     return (
         <>
+            <Navigation />
+
+            <Routes>
+                <Route path={"/"} element={<Homepage />} />
+                <Route path={"/charachters"} element={<Characters />} />
+            </Routes>
+
+
             <CharacterGallery  characters={characters}/>
         </>
     )
 }
-
 export default App
